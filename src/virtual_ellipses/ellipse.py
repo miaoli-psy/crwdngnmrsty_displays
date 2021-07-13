d28bc88f9e3509794eb2dc97cf78cb3b852d696e
miaoli-psy <chuoli223@yahoo.com>
2021-07-14 00:05:29 +0200

    update Ellipse: add ellipse polygon

--- src/virtual_ellipses/ellipse.py
+++ src/virtual_ellipses/ellipse.py
@@ -1,4 +1,6 @@
 from math import atan2, degrees
+from shapely.geometry import Point
+from shapely.affinity import scale, rotate
 
 
 class Ellipse:
@@ -10,8 +12,18 @@ class Ellipse:
     def get_radial_angle(self):
         return degrees(atan2(self.ellipse_center_coordinate[1], self.ellipse_center_coordinate[0]))
 
+    def get_polygon(self):
+        p = Point(self.ellipse_center_coordinate)
+        c = p.buffer(1)
+        ellipse_polygon = scale(c, self.ka, self.kb)
+        ellipse_polygon = rotate(geom = ellipse_polygon,
+                                 angle = self.get_radial_angle(),
+                                 origin = "centroid")
+        return ellipse_polygon
+
 
 if __name__ == '__main__':
     debug = True
     if debug:
-        e = Ellipse((1.732, 1), 3, 5)
+        e = Ellipse((1.732, 1), 2, 0.5)
+        e.get_polygon()
