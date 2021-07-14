8c8e70e3b30d410294757d03271477feacc13a09
miaoli-psy <chuoli223@yahoo.com>
2021-07-14 19:15:34 +0200

    add Positions class

--- src/positions/positions.py
+++ src/positions/positions.py
@@ -1,38 +1,46 @@
 import math
+import matplotlib.pyplot as plt
 from src.common.process_basic_data_structure import get_diff_between_2_lists
 
 
-def generate_all_posis(grid_x = 101, grid_y = 75, linelength = 10):
-    '''get full positions in a dimention of grid_x*grid_y'''
-    start_x = -0.5 * linelength * grid_x + 0.5 * linelength
-    start_y = -0.5 * linelength * grid_y + 0.5 * linelength
-    positions = []
-    for x in range(0, grid_x):
-        new_x = start_x + x * linelength
-        for y in range(0, grid_y):
-            new_y = start_y + y * linelength
-            positions.append((new_x, new_y))
-    try:
-        positions.remove((0, 0))  # remove the center
-    except ValueError:
-        pass
-    return positions
+class Positions:
+    def __init__(self, grid_x = 101, grid_y = 75, line_length = 10, fovea_radius = 100):
+        self.grid_x = grid_x
+        self.grid_y = grid_y
+        self.line_length = line_length
+        self.fovea_radius = fovea_radius
+        self.all_posis = self.generate_all_posis()
+        self.all_posis_no_fovea = self.get_all_posis_remove_fovea()
 
+    def generate_all_posis(self):
+        """get full positions in a dimension of grid_x*grid_y"""
+        start_x = -0.5 * self.line_length * self.grid_x + 0.5 * self.line_length
+        start_y = -0.5 * self.line_length * self.grid_y + 0.5 * self.line_length
+        positions = []
+        for x in range(0, self.grid_x):
+            new_x = start_x + x * self.line_length
+            for y in range(0, self.grid_y):
+                new_y = start_y + y * self.line_length
+                positions.append((new_x, new_y))
+        try:
+            positions.remove((0, 0))  # remove the center
+        except ValueError:
+            pass
+        return positions
 
-def get_fovea_posis(posilist, r = 100):
-    return [posi for posi in posilist if math.sqrt((posi[0] ** 2) + (posi[1] ** 2)) < r]
+    def fovea_posis(self):
+        return [posi for posi in self.all_posis if math.sqrt((posi[0] ** 2) + (posi[1] ** 2)) < self.fovea_radius]
 
-
-def remove_fovea_posis(r = 100, grid_x = 101, grid_y = 75, linelength = 10):
-    # initial positions
-    posis = generate_all_posis(grid_x = grid_x, grid_y = grid_y, linelength = linelength)
-    # fovia posis
-    fovea_posis = get_fovea_posis(posilist = posis, r = r)
-    return get_diff_between_2_lists(posis, fovea_posis)
+    def get_all_posis_remove_fovea(self):
+        return get_diff_between_2_lists(self.all_posis, self.fovea_posis())
 
 
 if __name__ == "__main__":
     debug = True
     if debug:
-        test_posis = generate_all_posis(grid_x = 10, grid_y = 7, linelength = 1)
-        left = remove_fovea_posis()
+        test_posis = Positions(grid_x = 101, grid_y = 75, line_length = 10, fovea_radius = 100)
+        fovea = test_posis.fovea_posis()
+        all = test_posis.all_posis_no_fovea
+        ax = plt.subplot()
+        for posi in all:
+            ax.plot(posi[0], posi[1], marker = ".", markersize = 1)
