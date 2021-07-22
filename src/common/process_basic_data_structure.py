a8b068fd3dd88502161eb594b62db54276517cd5
miaoli-psy <chuoli223@yahoo.com>
2021-07-22 16:25:50 +0200

    update display properties method

--- src/common/process_basic_data_structure.py
+++ src/common/process_basic_data_structure.py
@@ -1,3 +1,7 @@
 def get_diff_between_2_lists(list1, list2):
     li_dif = [i for i in list1 + list2 if i not in list1 or i not in list2]
     return li_dif
+
+
+def cal_average(lst):
+    return round(sum(lst) / len(lst), 2)
--- src/display_properties/properties.py
+++ src/display_properties/properties.py
@@ -2,57 +2,36 @@ from scipy.spatial import distance, ConvexHull
 from itertools import combinations
 import numpy as np
 
-
-def displayposi_to_array(posilist):
-    return np.asarray(posilist)
+from src.common.process_basic_data_structure import cal_average
 
 
-def get_hull(posilist):
-    return ConvexHull(displayposi_to_array(posilist))
+class Properties:
+    def __init__(self, posilist):
+        self.__posilist_array = np.asarray(posilist)
+        self.__hull = ConvexHull(self.__posilist_array)
+        self.convexhull = round(self.__hull.area * (0.25 / 3.82), 2)
+        self.occupancy_area = round(self.__hull.volume * ((0.25 / 3.82) ** 2), 2)
+        self.averge_eccentricity = self.cal_averge_eccentricity()
+        self.average_spacing = self.get_average_spacing()
+        self.density = round(len(posilist)/self.occupancy_area, 4)
 
+    def cal_averge_eccentricity(self):
+        all_eccentricity = [distance.euclidean(posi, (0, 0)) * (0.25 / 3.82) for posi in self.__posilist_array]
+        return cal_average(all_eccentricity)
 
-def cal_average(lst):
-    return round(sum(lst)/len(lst),2)
-
-
-def get_all_eccentricity(posilist):
-    posilist_array = displayposi_to_array(posilist)
-    return [distance.euclidean(posi, (0, 0))*(0.25/3.82) for posi in posilist_array]
-
-
-def get_convexhull(posilist):
-    posilist_array = displayposi_to_array(posilist)
-    hull = get_hull(posilist_array)
-    return round(hull.area*(0.25/3.82),2)
-
-
-def get_occupancy_area(posilist):
-    posilist_array = displayposi_to_array(posilist)
-    hull = get_hull(posilist_array)
-    return round(hull.volume*(((0.25/3.82)**2)),2)
-
-
-def cal_averge_eccentricity(posilist):
-    all_eccentricity = get_all_eccentricity(posilist)
-    return cal_average(all_eccentricity)
-
-
-def get_average_spacing(posilist):
-    posilist_array = displayposi_to_array(posilist)
-    distances =[distance.euclidean(p1,p2) for p1, p2 in combinations(posilist_array,2)]
-    return round(sum(distances)/len(distances)*(0.25/3.82),2)
+    def get_average_spacing(self):
+        distances = [distance.euclidean(p1, p2) for p1, p2 in combinations(self.__posilist_array, 2)]
+        return round(sum(distances) / len(distances) * (0.25 / 3.82), 2)
 
 
 if __name__ == "__main__":
-    test = False
-    if test:
-        posilist = [(20, 0), (25, 5), (100, 75), (50, 50), (-75, 65)]
-
-        a = get_convexhull(posilist)
-        b = get_occupancy_area(posilist)
-        c = cal_averge_eccentricity(posilist)
-        d = get_average_spacing(posilist)
-
-        print("convexhull is", a, ";", "occupancy area is", b, ";", "average eccentricity is", c, ";", "and average spacing is", d)
-
-
+    debug = True
+    if debug:
+        properties = Properties([(20, 0), (25, 5), (100, 75), (50, 50), (-75, 65)])
+        a = properties.convexhull
+        b = properties.occupancy_area
+        c = properties.averge_eccentricity
+        d = properties.average_spacing
+        e = properties.density
+        print("convexhull is", a, ";", "occupancy area is", b, ";", "average eccentricity is", c, ";",
+              "and average spacing is", d, "density is", e, "item/cm2")
