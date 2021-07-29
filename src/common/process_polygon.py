7be95e98458271246cfd40eb5a2396235dffaed2
miaoli-psy <chuoli223@yahoo.com>
2021-07-29 21:12:21 +0200

    plot polygon, choose random posi on polygon

---
+++ src/common/process_polygon.py
@@ -0,0 +1,29 @@
+import random
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
+from descartes import PolygonPatch
+
+
+def get_random_point_in_polygon(poly):
+    minx, miny, maxx, maxy = poly.bounds
+    while True:
+        point = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
+        if poly.contains(point):
+            return point
+
+
+def plot_polygon(polygonlist, axes_lim = None):
+    if axes_lim is None:
+        axes_lim = [-20, 20]
+    ax = plt.subplot()
+    for poly in polygonlist:
+        patch = PolygonPatch(poly)
+        ax.add_artist(patch)
+    ax.set_xlim(axes_lim)
+    ax.set_ylim(axes_lim)
+    ax.set_aspect('equal', 'box')
+    plt.show()
+
+
+def get_intersect_poly(poly1, poly2):
+    return poly1.intersection(poly2)
