f0cac9f086114d25b093f6982a85590f592d42b1
miaoli-psy <chuoli223@yahoo.com>
2021-07-30 12:50:22 +0200

    bug fixed, 75% pairs done

--- src/displays.py
+++ src/displays.py
@@ -16,7 +16,7 @@ all_posis_object = AllPositions(grid_x = 101, grid_y = 75, line_length = 10, fov
 full_posi_list = all_posis_object.get_all_posi_in_winsize(winsize = curr_winsize)
 
 
-def get_extra_random_posi(based_posis, ori = "radial"):
+def get_one_extra_random_posis(based_posis, ori = "radial"):
     extra_posis = list()
     half_posis_a, half_posis_b = random_split_list(based_posis, weight = 0.5)
     for posi in half_posis_a:
@@ -34,29 +34,45 @@ def get_extra_random_posi(based_posis, ori = "radial"):
     return extra_posis
 
 
+def get_two_extra_posis(based_posis, ori = "radial"):
+    extra_posis = list()
+    for posi in based_posis:
+        if ori == "radial":
+            point = get_close_to_fovea_posi(posi)
+            extra_posis.append(polypoint_to_coords(point))
+            point = get_far_from_fovea_posi(posi)
+            extra_posis.append(polypoint_to_coords(point))
+        elif ori == "tangential":
+            point = get_posi_in_tan_area_a(posi)
+            extra_posis.append(polypoint_to_coords(point))
+            point = get_posi_in_tan_area_b(posi)
+            extra_posis.append(polypoint_to_coords(point))
+    return extra_posis
+
+
 def gen_display_full_pairs(based_posis, protect_zone_ori = "radial"):
-    return based_posis, get_extra_random_posi(based_posis, ori = protect_zone_ori)
+    return based_posis, get_one_extra_random_posis(based_posis, ori = protect_zone_ori)
 
 
 def gen_display_75p_paris(based_posis, protect_zone_ori = "radial"):
     extra_posis = list()
-    no_extra_posis = random_split_list(based_posis, weight = 0.125)[0]
-    rest_posis = get_diff_between_2_lists(based_posis, no_extra_posis)
-    two_extra_posis = random.sample(rest_posis, len(no_extra_posis))
-    for posi in two_extra_posis:
-        point = get_close_to_fovea_posi(posi)
-        extra_posis.append(polypoint_to_coords(point))
-        point = get_far_from_fovea_posi(posi)
-        extra_posis.append(polypoint_to_coords(point))
-    one_extra_posis = get_diff_between_2_lists(rest_posis, two_extra_posis)
-    extra_posis_list = get_extra_random_posi(based_posis = one_extra_posis, ori = protect_zone_ori)
-    extra_posis = extra_posis + extra_posis_list
+    # single based disc posis 只有中间一个点
+    no_extra_posi_base = random_split_list(based_posis, weight = 0.125)[0]
+    # other posis (2 extra posis, and 1 extra posi)
+    rest_posis = get_diff_between_2_lists(based_posis, no_extra_posi_base)
+    # the number of 2 extra posis == no extra posis
+    two_extra_posis_base = random.sample(rest_posis, len(no_extra_posi_base))
+    # get 2 extra posis list
+    two_extra_posis_list = get_two_extra_posis(based_posis = two_extra_posis_base, ori = protect_zone_ori)
+    # get 1 extra posis list
+    one_extra_posi_base = get_diff_between_2_lists(rest_posis, two_extra_posis_base)
+    extra_posis_list = get_one_extra_random_posis(based_posis = one_extra_posi_base, ori = protect_zone_ori)
+    extra_posis = extra_posis + extra_posis_list + two_extra_posis_list
 
     return based_posis, extra_posis
 
 
 if __name__ == '__main__':
     based_posis = get_display(full_posi_list, protect_zone_ori = protect_zone_ori)
-    display = gen_display_75p_paris(based_posis, protect_zone_ori = "tangential")
+    display = gen_display_75p_paris(based_posis, protect_zone_ori = "radial")
     drawEllipse_full(display[0], display[1], ka = 0.25, kb = 0.1, ellipseColor_t = "white", ellipseColor_r = "white")
-
