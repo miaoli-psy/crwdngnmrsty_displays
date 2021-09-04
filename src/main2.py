44e61fcfdfe1dfaf2c685513d347aabf9f53d43a
miaoli-psy <chuoli223@yahoo.com>
2021-09-04 23:02:18 +0200

    add main2, get displays that protect zones are circles

--- src/main2.py
+++ src/main2.py
@@ -7,22 +7,22 @@ from src.process_plane import get_display
 from src.properties import Properties
 
 curr_winsize = 0.4
-protect_zone_type = "radial"
-percent_pairs = 1
+protect_zone_type = "circle"
 save_to_csv = False
+radial_weight = 0.15
+tan_weight = 0.15
 
-run_n = 5
+run_n = 5000
 all_posis_object = AllPositions(grid_x = 101, grid_y = 75, line_length = 10, fovea_radius = 100)
 full_posi_list = all_posis_object.get_all_posi_in_winsize(winsize = curr_winsize)
-column_names = ["n", "winsize", "numerosity", "all posis", "central posis", "extra posis", "percept pairs",
-                "protect zone type", "convexhull", "occupancy area", "average eccentricity", "average spacing",
-                "density"]
+column_names = ["n", "winsize", "numerosity", "all posis", "extra posis", "protect zone type", "convexhull",
+                "occupancy area", "average eccentricity", "average spacing", "density", "radius"]
 
 all_display_df = pd.DataFrame(columns = column_names)
 for n in range(1, run_n + 1):
     print(n)
-    all_based_posis = get_display(full_posi_list, protect_zone_ori = Orientation.Radial, radial_weight = 0.15,
-                                  tan_weight = 0.15)
+    all_based_posis = get_display(full_posi_list, protect_zone_ori = Orientation.Radial,
+                                  radial_weight = radial_weight, tan_weight = tan_weight)
 
     properites = Properties(all_based_posis)
 
@@ -30,17 +30,15 @@ for n in range(1, run_n + 1):
                    "winsize":              curr_winsize,
                    "numerosity":           len(all_based_posis),
                    "all posis":            all_based_posis,
-                   "central posis":        all_based_posis,
-                   "extra posis":          0,
-                   "percept pairs":        percent_pairs,
                    "protect zone type":    protect_zone_type,
                    "convexhull":           properites.convexhull,
                    "occupancy area":       properites.occupancy_area,
                    "average eccentricity": properites.averge_eccentricity,
                    "average spacing":      properites.average_spacing,
-                   "density":              properites.density}
+                   "density":              properites.density,
+                   "radius":               radial_weight}
 
     all_display_df = all_display_df.append(new_display, ignore_index = True)
 
 if save_to_csv:
-    all_display_df.to_csv("ws%s_%s_%spairs.csv" % (curr_winsize, protect_zone_type, percent_pairs), index = False)
+    all_display_df.to_csv("ws%s_%s_%s.csv" % (curr_winsize, protect_zone_type, radial_weight), index = False)
