01172216fc05179ee7c5c0a087aaff0e3f0f66f5
miaoli-psy <chuoli223@yahoo.com>
2021-10-11 18:04:06 +0200

    generate triplets displays

--- src/displays.py
+++ src/displays.py
@@ -2,7 +2,7 @@ from allpositions import AllPositions
 from ellipse import Orientation
 from src.common.process_basic_data_structure import random_split_list, get_diff_between_2_lists
 from src.common.process_polygon import polypoint_to_coords
-from src.draw_displays import drawEllipse_full, draw_disc_only
+from src.draw_displays import drawEllipse_full, draw_disc_only, drawEllipses
 from src.process_plane import get_display, get_close_to_fovea_posi, get_far_from_fovea_posi, get_posi_in_tan_area_a, \
     get_posi_in_tan_area_b
 import random
@@ -83,13 +83,16 @@ if __name__ == '__main__':
     debug = True
     if debug:
         curr_winsize = 0.6
-        protect_zone_ori = Orientation.Both
+        protect_zone_ori = Orientation.Radial
         protect_zone_type = "radial"
         percent_pairs = 0
         all_posis_object = AllPositions(grid_x = 101, grid_y = 75, line_length = 10, fovea_radius = 100)
         full_posi_list = all_posis_object.get_all_posi_in_winsize(winsize = curr_winsize)
         all_based_posis = get_display(full_posi_list, protect_zone_ori = protect_zone_ori)
-        display = generate_display_add_one_extra_discs(all_based_posis, percent_pairs = percent_pairs, add_discs_ori = protect_zone_type)
-        drawEllipse_full(display[0], display[1], ka = 0.25, kb = 0.1, ellipseColor_t = "white", ellipseColor_r = "white")
-        draw_disc_only(display[0], display[1], contrast = True)
+        # display = generate_display_add_one_extra_discs(all_based_posis, percent_pairs = percent_pairs, add_discs_ori = protect_zone_type)
+        extras = get_two_extra_posis(based_posis = all_based_posis, ori = protect_zone_type)
+        display = [all_based_posis, extras]
+        # drawEllipse_full(display[0], display[1], ka = 0.25, kb = 0.1, ellipseColor_t = "white", ellipseColor_r = "white")
+        drawEllipses(all_based_posis, ka = 0.25, kb = 0.1, ellipseColor = "white", extra_posi = extras)
+        # draw_disc_only(display[0], display[1], contrast = True)
         draw_disc_only(display[0], display[1], contrast = False)
--- src/main3.py
+++ src/main3.py
@@ -2,47 +2,45 @@ import pandas as pd
 
 from ellipse import Orientation
 from src.allpositions import AllPositions
-from src.displays import generate_display_direct_add_extra_discs
+from src.displays import generate_display_add_one_extra_discs, get_two_extra_posis
 from src.process_plane import get_display
 from src.properties import Properties
 
 curr_winsize = 0.4
 protect_zone_type = "radial"
-percent_pairs = 1
-save_to_csv = False
+save_to_csv = True
 
-run_n = 500
+run_n = 5
 all_posis_object = AllPositions(grid_x = 101, grid_y = 75, line_length = 10, fovea_radius = 100)
 full_posi_list = all_posis_object.get_all_posi_in_winsize(winsize = curr_winsize)
-column_names = ["n", "winsize", "numerosity", "all posis", "central posis", "extra posis", "percept pairs",
-                "protect zone type", "convexhull", "occupancy area", "average eccentricity", "average spacing",
-                "density"]
+column_names = ["n", "winsize", "numerosity", "allposis", "centralposis", "extraposis", "protectzonetype",
+                "convexhull", "occupancyarea", "averageeccentricity", "averagespacing", "density"]
 
 all_display_df = pd.DataFrame(columns = column_names)
 for n in range(1, run_n + 1):
     print(n)
     all_based_posis = get_display(full_posi_list, protect_zone_ori = Orientation.Both)
-    my_display = generate_display_direct_add_extra_discs(based_posis = all_based_posis,
-                                                         percent_pairs = percent_pairs,
-                                                         add_discs_ori = protect_zone_type)
+
+    extra_posis = get_two_extra_posis(based_posis = all_based_posis, ori = protect_zone_type)
+
+    my_display = [all_based_posis, extra_posis]
 
     properites = Properties(my_display[0] + my_display[1])
 
-    new_display = {"n":                    n,
-                   "winsize":              curr_winsize,
-                   "numerosity":           len(my_display[0]) * 2,
-                   "all posis":            my_display[0] + my_display[1],
-                   "central posis":        my_display[0],
-                   "extra posis":          my_display[1],
-                   "percept pairs":        percent_pairs,
-                   "protect zone type":    protect_zone_type,
-                   "convexhull":           properites.convexhull,
-                   "occupancy area":       properites.occupancy_area,
-                   "average eccentricity": properites.averge_eccentricity,
-                   "average spacing":      properites.average_spacing,
-                   "density":              properites.density}
+    new_display = {"n":                   n,
+                   "winsize":             curr_winsize,
+                   "numerosity":          len(my_display[0]) * 2,
+                   "allposis":            my_display[0] + my_display[1],
+                   "centralposis":        my_display[0],
+                   "extraposis":          my_display[1],
+                   "protectzonetype":     protect_zone_type,
+                   "convexhull":          properites.convexhull,
+                   "occupancyarea":       properites.occupancy_area,
+                   "averageeccentricity": properites.averge_eccentricity,
+                   "averagespacing":      properites.average_spacing,
+                   "density":             properites.density}
 
     all_display_df = all_display_df.append(new_display, ignore_index = True)
 
 if save_to_csv:
-    all_display_df.to_csv("ws%s_%s_%spairs.csv" % (curr_winsize, protect_zone_type, percent_pairs), index = False)
+    all_display_df.to_csv("ws%s_%striplets.csv" % (curr_winsize, protect_zone_type), index = False)
