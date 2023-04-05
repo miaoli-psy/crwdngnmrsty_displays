8cb7600ca8c25808659d5568187ebc7306339064
miao <chuoli223@yahoo.com>
2023-04-05 14:36:21 +0200

    random select N discs, add save to excel

--- src/main4.py
+++ src/main4.py
@@ -8,16 +8,16 @@ import pandas as pd
 from src.allpositions import AllPositions
 from src.properties import Properties
 
-curr_winsize = 0.5
-save_to_csv = True
+radius = 200
+save_data = False
 
 run_n = 1000
 numerosity = 16
 
-all_posis_object = AllPositions(grid_x = 75, grid_y = 75, line_length = 10, fovea_radius = 2)
-full_posi_list = all_posis_object.get_all_posi_in_winsize(winsize = curr_winsize)
+all_posis_object = AllPositions(grid_x = 75, grid_y = 75, line_length = 20, fovea_radius = 1)
+full_posi_list = all_posis_object.get_all_posi_in_circular(radius = radius)
 
-column_names = ["n", "winsize", "numerosity", "allposis",  "convexhull",
+column_names = ["n", "numerosity", "radius", "allposis",  "convexhull",
                 "occupancyarea", "averageeccentricity", "averagespacing", "density"]
 
 all_display_df = pd.DataFrame(columns = column_names)
@@ -30,7 +30,7 @@ for n in range(1, run_n + 1):
     properites = Properties(my_posis)
 
     new_display = {"n": n,
-                   "winsize": curr_winsize,
+                   "radius": radius,
                    "numerosity": numerosity,
                    "allposis": my_posis,
                    "convexhull": properites.convexhull,
@@ -41,5 +41,6 @@ for n in range(1, run_n + 1):
 
     all_display_df = all_display_df.append(new_display, ignore_index = True)
 
-if save_to_csv:
+if save_data:
     all_display_df.to_csv("n%s_%s.csv" % (n, numerosity), index = False)
+    all_display_df.to_excel("n%s_%s.xlsx" % (n, numerosity), index = False)
