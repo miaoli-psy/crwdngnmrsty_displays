77dabed1ca71d8f725061c9b5c052e48b00b303f
miao <chuoli223@yahoo.com>
2025-04-18 20:52:07 +0200

    generate sectors displays

--- src/main_local_sector.py
+++ src/main_local_sector.py
@@ -1,4 +1,6 @@
+import os
 import random
+from tqdm import tqdm
 import pandas as pd
 from ellipse import Ellipse, Orientation
 from src.process_plane import get_display
@@ -9,29 +11,34 @@ from src.allpositions import AllPositions_full
 from src.properties import Properties
 
 
-curr_winsize = 0.6
-sector_angle = 90 # 0-360
+curr_winsize = 1
+sector_angle = 170 # 0-360
 direction = 0 # 0-360
-fovea_radius = 200
+fovea_radius = 100 # fovea where no disc is allowed
+circular_region_radius = 512 # circular region allowed discs
 radial_weight = 0.25
 tangential_weight = 0.1
-protect_zone_type = "radial"
-# protect_zone_type = "tangential"
+# protect_zone_type = "radial"
+protect_zone_type = "tangential"
 save_to_csv = True
+savefig = False
 demo_plots = True
-run_n = 10
+run_n = 5
 
 all_posis_Object = AllPositions_full(width = 1920, height = 1080, fovea_radius = 200, window_size = curr_winsize)
-full_posi_list = all_posis_Object.generate_sector_posi(angle_deg = sector_angle, direction_deg = direction)
+filter_circular_posis = all_posis_Object.generate_posi_in_circle(radius = circular_region_radius)
+
+full_posi_list = all_posis_Object.generate_sector_posi(angle_deg = sector_angle, direction_deg = direction, all_positions = filter_circular_posis)
 
 
 column_names = ["n", "winsize", "numerosity", "allposis", "centralposis", "extraposis", "protectzonetype",
                 "convexhull", "occupancyarea", "averageeccentricity", "averagespacing", "density"]
 
-all_display_df = pd.DataFrame(columns = column_names)
+# all_display_df = pd.DataFrame(columns = column_names)
 
-for n in range(1, run_n + 1):
-    print(n)
+rows = []
+for n in tqdm(range(1, run_n + 1), desc="Generating Displays"): # 加入进度条
+    # print(n)
     base_posis = get_display(full_posi_list, protect_zone_ori = Orientation.Both, radial_weight = radial_weight, tan_weight = tangential_weight)
     extra_posis = get_two_extra_posis(based_posis = base_posis, ori = protect_zone_type)
 
@@ -51,16 +58,24 @@ for n in range(1, run_n + 1):
                    "averageeccentricity": properites.averge_eccentricity,
                    "averagespacing":      properites.average_spacing,
                    "density":             properites.density}
+    rows.append(new_display)
 
-    all_display_df = pd.concat([all_display_df, pd.DataFrame([new_display])], ignore_index=True)
+# all_display_df = pd.concat([all_display_df, pd.DataFrame([new_display])], ignore_index=True)
+all_display_df = pd.DataFrame(rows, columns=column_names)
 
 
 if save_to_csv:
-    all_display_df.to_csv("ws%s_%s_angle%s_drctn%s.csv" % (curr_winsize, protect_zone_type, sector_angle, direction), index = False)
+    # curr dir
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # file name
+    file_name = "ws%s_%s_angle%s_drctn%s.csv" % (curr_winsize, protect_zone_type, sector_angle, direction)
+    save_path = os.path.join(current_dir, file_name)
+    all_display_df.to_csv(save_path, index = False)
 
 if demo_plots:
-    savefig = True
-    i = 1  # choose the row index you want to plot, it's coln-1
+
+    i = 0  # choose the row index you want to plot, it's coln-1
     row = all_display_df.iloc[i]
 
     base_posis = row['centralposis']
